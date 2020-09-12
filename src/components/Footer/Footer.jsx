9ee970ebdb2849b358d23a987b148bb7d21d223c
Lala <luangu2020@gmail.com>
2020-09-12 13:39:07 -0400

    fixing portfolio

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
 import GithubButtons from '../GithubButtons/GithubButtons';
 
-import { githubButtons } from '../../mock/data';
+// import { githubButtons } from '../../mock/data';
 
 const Footer = () => {
   const { footer } = useContext(PortfolioContext);
   const { networks } = footer;
-  const { isEnabled } = githubButtons;
+  const { isEnabled } = GithubButtons;
 
   return (
     <footer className="footer navbar-static-bottom">
--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,67 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'LAGG', // e.g: 'Name | Developer'
+  lang: 'EN, ES', // e.g: en, es, fr, jp
+  description: 'I am glad you are here', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'L. Angela Gutierrez',
+  subtitle: 'I am ready to transition into Web Development and Design',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'IMG_2670.JPG',
+  paragraphOne: 'As a professional with a background in the design of living spaces with an interest in function over form,\nI bring those qualities to the web development area. My interest is creating  thorough and user friendly applications that also have elements of good design. For me, functionality  and creating a good user experience is first  and foremost the objective.',
+  paragraphTwo: 'I recently received a certificate in Web Development by the University of New Hampshire, powered by Trilogy 2U education. I am proficient in HTML5, CSS, JavaScript, RESTful API, Node.js, PWA, Express, Mongo and MySQL and React and Vue frameworks',
+  paragraphThree: 'Always interested in learning new technologies and broaden my skill set',
+  resume: 'LAGGResume.pdf', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'RecipeBoxHP.PNG',
+    title: 'Recipe Box',
+    info: 'Save your recipes in an online database',
+    info2: 'Node, Bootstrap, Express, MongoDB',
+    url: 'https://fast-mountain-43531.herokuapp.com/index.html',
+    repo: 'https://github.com/Kaleighspurio/Project2-Recipe-Box', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'homepage.jpg',
+    title: 'Project-1-Professor-Planet',
+    info: 'Professor Planet is here to teach you about the solar system. Give it a try!',
+    info2: 'Materialize, API calls',
+    url: 'ttps://github.com/lalaggv2/Project-1-Professor-Planet',
+    repo: 'https://bowdwin.github.io/Project-1/', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'Workout2.png',
+    title: 'Workout Tracker',
+    info:
+      'Keep track of your workout, either cardio or resistance. Build up and see the charts denoting your progress!!!',
+    info2: 'JavaScript, CSS, HTML, MongoDB, Node.js, Express',
+    url: 'https://workout-tracker2020nh.herokuapp.com/?id=5f2751caa0b2ac0017ec567e',
+    repo: 'https://github.com/lalaggv2/workoutrack', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Budget1.png',
+    title: 'Offline Budget Tracker',
+    info:
+      'Keep track of your expenses even when offline, and be able to update them when back online to the database',
+    info2: 'Node, Bootstrap, Express, IndexedDB, PWA',
+    url: 'https://fast-mountain-43531.herokuapp.com/index.html',
+    repo: 'https://github.com/Kaleighspurio/Project2-Recipe-Box', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +70,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'luangu2020@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +78,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/lalagg',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/luz-angela-g-3256769b/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/lalaggv2',
     },
   ],
-};
-
-// Github start/fork buttons
-export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
 };
