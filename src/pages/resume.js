5c8cb235cb73f0b25b5ec4031da12b66c55c78ab
Lala <luangu2020@gmail.com>
2020-09-13 12:43:12 -0400

    fixing eslint

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -11,9 +11,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Portfolio LA Gutierrez'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Portfolio LA Gutierrez'} />
       </Helmet>
       <embed src="/assets/LAGGResume.pdf" width="100%" height="100%" />
     </>
